acbe95f189b70e255525f7b94b0bf2b43a8b3ff4
Kenneth Amiel Santos <kennethamiel.santos@gmail.com>
2025-01-03 18:10:23 +0800

    Added RenderModel Component and Initial Homepage Background

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,11 @@ import bg from "../../public/background/home-background.png"
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between relative">
-      <Image src={bg} alt="background-image" className="w-full h-flll object-cover object-center opacity-25"/>
+      <Image src={bg} alt="background-image" className="w-full h-full object-cover object-center opacity-25"/>
+
+      <div className="w-full h-screen">  
+        {/* navigation and 3d model */}
+      </div>
     </main>
   );
 }
