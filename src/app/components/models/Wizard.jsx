cf6c02387499566b4077816f65f66c153d2888f9
Kenneth Amiel Santos <kennethamiel.santos@gmail.com>
2025-01-03 18:37:01 +0800

    Added Initial Homepage Wizard Model

--- src/app/components/models/Wizard.jsx
+++ src/app/components/models/Wizard.jsx
@@ -11,6 +11,7 @@ export default function Wizard(props) {
     <group {...props} dispose={null}
         position={[0, -1.5, 0]}
         scale={[0.06, 0.06, 0.06]}
+        rotation={[0.25, 0, 0]}
     >
       <mesh
         castShadow
