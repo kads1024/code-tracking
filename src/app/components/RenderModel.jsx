c3d295b64e50a1719cc1ba26a56417d0c0c31f9d
Kenneth Amiel Santos <kennethamiel.santos@gmail.com>
2025-01-03 18:54:42 +0800

    Fix Formatting for page.js and RenderModel.jsx

--- src/app/components/RenderModel.jsx
+++ src/app/components/RenderModel.jsx
@@ -4,20 +4,23 @@ import { Canvas } from '@react-three/fiber'
 import clsx from 'clsx'
 import React, { Suspense } from 'react'
 
-const RenderModel = ({children, className}) => {
+const RenderModel = ({ children, className }) => {
+    return (
+        // Canvas is a wrapper for the 3D scene
+        <Canvas
+            // clsx is a utility for conditionally joining class names together
+            className={clsx("w-screen h-screen -z-10 relative", className)}
+        >
+            {/* Suspense is a wrapper for the 3D model */}
+            <Suspense fallback={null}>
+                {children}
+            </Suspense>
 
-
-
-  return (
-    <Canvas
-      className={clsx("w-screen h-screen -z-10 relative", className)}
-      >
-        <Suspense fallback={null}>
-            {children}
-        </Suspense>
-        <Environment preset="dawn" />
-    </Canvas>
-  )
+            {/* Environment is a wrapper for the lighting */}
+            <Environment preset="dawn" />
+        </Canvas>
+    )
 }
 
+// Export the component
 export default RenderModel
--- src/app/page.js
+++ src/app/page.js
@@ -6,9 +6,9 @@ import Wizard from "./components/models/Wizard";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between relative">
-      <Image src={bg} alt="background-image" fill className="-z-50 w-full h-full object-cover object-center opacity-25"/>
+      <Image src={bg} alt="background-image" fill className="-z-50 w-full h-full object-cover object-center opacity-25" />
 
-      <div className="w-full h-screen">  
+      <div className="w-full h-screen">
         {/* navigation and 3d model */}
         <RenderModel>
           <Wizard />
