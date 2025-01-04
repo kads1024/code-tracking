3c80c9e781ef13dbd23fa303bf31d1ecf28a0516
Kenneth Amiel Santos <kennethamiel.santos@gmail.com>
2025-01-04 17:41:01 +0800

    DevTrack: Files: FirefliesBackground.jsx, layout.e28b297ac70823c1.hot-update.js, e28b297ac70823c1.webpack.hot-update.json, webpack.e28b297ac70823c1.hot-update.js, react-loadable-manifest.json, app-build-manifest.json, build-manifest.json, page_client-reference-manifest.js, middleware-build-manifest.js, middleware-react-loadable-manifest.js, next-font-manifest.js, next-font-manifest.json, layout.js, webpack.js, _buildManifest.js, _ssgManifest.js, 4c69a713848bd706.webpack.hot-update.json, layout.4c69a713848bd706.hot-update.js, webpack.4c69a713848bd706.hot-update.js, 99aeb30fbce5a0ff.webpack.hot-update.json, layout.99aeb30fbce5a0ff.hot-update.js, webpack.99aeb30fbce5a0ff.hot-update.js, 964b30712f607915.webpack.hot-update.json, layout.964b30712f607915.hot-update.js, webpack.964b30712f607915.hot-update.js, 8ce8b2673fb82dac.webpack.hot-update.json, layout.8ce8b2673fb82dac.hot-update.js, webpack.8ce8b2673fb82dac.hot-update.js, app-paths-manifest.json, page.js, pages-manifest.json, server-reference-manifest.js, server-reference-manifest.json, webpack-runtime.js, layout.ts, page.ts, package.json, 066e97dc87c1ad5a.webpack.hot-update.json, layout.066e97dc87c1ad5a.hot-update.js, webpack.066e97dc87c1ad5a.hot-update.js, layout.css, 16f6040025b7b22f.webpack.hot-update.json, layout.16f6040025b7b22f.hot-update.js, webpack.16f6040025b7b22f.hot-update.js. [develop]  | Changes in: FirefliesBackground.jsx (added import, className); layout.e28b297ac70823c1.hot-update.js (added import, className); e28b297ac70823c1.webpack.hot-update.json (added import, className); webpack.e28b297ac70823c1.hot-update.js (added import, className); react-loadable-manifest.json; app-build-manifest.json; build-manifest.json; page_client-reference-manifest.js; middleware-build-manifest.js; middleware-react-loadable-manifest.js; next-font-manifest.js; next-font-manifest.json; layout.js; webpack.js; _buildManifest.js; _ssgManifest.js; 4c69a713848bd706.webpack.hot-update.json (added import, className); layout.4c69a713848bd706.hot-update.js (added import, className); webpack.4c69a713848bd706.hot-update.js (added import, className); 99aeb30fbce5a0ff.webpack.hot-update.json (added import, className); layout.99aeb30fbce5a0ff.hot-update.js (added import, className); webpack.99aeb30fbce5a0ff.hot-update.js (added import, className); 964b30712f607915.webpack.hot-update.json (added import, className); layout.964b30712f607915.hot-update.js (added import, className); webpack.964b30712f607915.hot-update.js (added import, className); layout.js; 8ce8b2673fb82dac.webpack.hot-update.json (added import, className); layout.8ce8b2673fb82dac.hot-update.js (added import, className); webpack.8ce8b2673fb82dac.hot-update.js (added import, className); app-paths-manifest.json; page.js; pages-manifest.json; server-reference-manifest.js; server-reference-manifest.json; webpack-runtime.js; layout.ts; page.ts; package.json; 066e97dc87c1ad5a.webpack.hot-update.json (added import, className); layout.066e97dc87c1ad5a.hot-update.js (added import, className); webpack.066e97dc87c1ad5a.hot-update.js (added import, className); layout.css; 16f6040025b7b22f.webpack.hot-update.json (added import, className); layout.16f6040025b7b22f.hot-update.js (added import, className); webpack.16f6040025b7b22f.hot-update.js (added import, className)

---
+++ src/app/components/FirefliesBackground.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FirefliesBackground = () => {
+    return (
+        <div className='fixed top-0 left-0 w-full h-full -z-10 overflow-hidden'>
+
+        </div>
+    )
+}
+
+export default FirefliesBackground
