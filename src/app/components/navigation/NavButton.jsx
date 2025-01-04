3041c69f66621bef00cd9eeee76d951a1a201d51
Kenneth Amiel Santos <kennethamiel.santos@gmail.com>
2025-01-04 15:23:34 +0800

    Added Proper navigation buttons

--- src/app/components/navigation/NavButton.jsx
+++ src/app/components/navigation/NavButton.jsx
@@ -1,5 +1,29 @@
+import { Home, Linkedin, Github, Palette, Phone, Twitter, User, NotebookText } from 'lucide-react'
 import Link from 'next/link'
 import React from 'react'
+
+const getIcon = (icon) => {
+    switch (icon) {
+        case "home":
+            return <Home className='w-full h-auto' strokeWidth={1.5} />;
+        case "about":
+            return <User className='w-full h-auto' strokeWidth={1.5} />;
+        case "projects":
+            return <Palette className='w-full h-auto' strokeWidth={1.5} />;
+        case "contact":
+            return <Phone className='w-full h-auto' strokeWidth={1.5} />;
+        case "github":
+            return <Github className='w-full h-auto' strokeWidth={1.5} />;
+        case "linkedin":
+            return <Linkedin className='w-full h-auto' strokeWidth={1.5} />;
+        case "twitter":
+            return <Twitter className='w-full h-auto' strokeWidth={1.5} />;
+        case "resume":
+            return <NotebookText className='w-full h-auto' strokeWidth={1.5} />;
+        default:
+            return <Home className='w-full h-auto' strokeWidth={1.5} />
+    }
+}
 
 const NavButton = ({ x, y, label, link, icon, newTab }) => {
     return (
@@ -10,14 +34,14 @@ const NavButton = ({ x, y, label, link, icon, newTab }) => {
                     transform: `translate(${x}, ${y})`
                 }
             }>
-            <Link 
-            href={link}
-            target={newTab ? '_blank' : '_self'}
-            className='text-foreground rounded-full flex items-center justify-center' 
-            aria-label={label}
-            name={label}>
-                <span>
-                    {icon}
+            <Link
+                href={link}
+                target={newTab ? '_blank' : '_self'}
+                className='text-foreground rounded-full flex items-center justify-center bg-background/20 border border-accent/30 border-solid backdrop-blur-[6px] shadow-glass-inset hover:shadow-glass-sm'
+                aria-label={label}
+                name={label}>
+                <span className='relative w-14 h-14 p-4'>
+                    {getIcon(icon)}
                 </span>
             </Link>
         </div>
