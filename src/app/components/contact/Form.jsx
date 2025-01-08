72ec8323b435f52ca9950116e6239715eb550418
Kenneth Amiel Santos <kennethamiel.santos@gmail.com>
2025-01-09 03:13:02 +0800

    Added toast message for submission of messages

--- src/app/components/contact/Form.jsx
+++ src/app/components/contact/Form.jsx
@@ -2,12 +2,12 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import emailjs from "@emailjs/browser";
-import {Toaster, toast } from 'sonner';
+import { Toaster, toast } from 'sonner';
 
 export default function Form() {
     const { register, handleSubmit, formState: { errors } } = useForm();
 
-    
+
 
     const sendEmail = (params) => {
         const toastId = toast.loading("Sending your message, please wait...");
@@ -24,10 +24,10 @@ export default function Form() {
                 })
             .then(
                 () => {
-                    console.log('SUCCESS!');
+                    toast.success("The wizard has received your message, he will get back to you soon!", { id: toastId })
                 },
                 (error) => {
-                    console.log('FAILED...', error.text);
+                    toast.error("There was an error casting your message, please try again later", { id: toastId })
                 },
             );
     };
@@ -48,47 +48,50 @@ export default function Form() {
 
 
         <>
-        <Toaster richColors={true} />
-         <form onSubmit={handleSubmit(onSubmit)}
-            className='max-w-md w-full flex flex-col items-center justify-center space-y-4'
-        >
-            <input type="text" placeholder="Name" {...register("Name", { required: 'This field is required!',
-                minLength: {
-                    value: 3,
-                    message: "Name should be at least 3 characters long."
+            <Toaster richColors={true} />
+            <form onSubmit={handleSubmit(onSubmit)}
+                className='max-w-md w-full flex flex-col items-center justify-center space-y-4'
+            >
+                <input type="text" placeholder="Name" {...register("Name", {
+                    required: 'This field is required!',
+                    minLength: {
+                        value: 3,
+                        message: "Name should be at least 3 characters long."
+                    }
+                })}
+                    className='w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg'
+                />
+                {
+                    errors.Name && <span className='inline-block self-start text-accent'>{errors.Name.message}</span>
                 }
-            })}
-                className='w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg'
-            />
-            {
-                errors.Name && <span className='inline-block self-start text-accent'>{errors.Name.message}</span>
-            }
 
-            <input type="email" placeholder="Email" {...register("Email", { required: 'This field is required!', pattern: /^\S+@\S+$/i })}
-                className='w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg'
-            />
-            {
-                errors.Email && <span className='inline-block self-start text-accent'>{errors.Email.message}</span>
-            }
-            <textarea placeholder='Message' 
-                {...register("Message", { required: 'This field is required!', maxLength: {
-                    value: 500,
-                    message: "Message should be less than 500 characters"
-                }, minLength: {
-                    value: 50,
-                    message: "Message should be at least 50 characters"
-                } })}
-                className='w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg'
-            />
-            {
-                errors.Message && <span className='inline-block self-start text-accent'>{errors.Message.message}</span>
-            }
+                <input type="email" placeholder="Email" {...register("Email", { required: 'This field is required!', pattern: /^\S+@\S+$/i })}
+                    className='w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg'
+                />
+                {
+                    errors.Email && <span className='inline-block self-start text-accent'>{errors.Email.message}</span>
+                }
+                <textarea placeholder='Message'
+                    {...register("Message", {
+                        required: 'This field is required!', maxLength: {
+                            value: 500,
+                            message: "Message should be less than 500 characters"
+                        }, minLength: {
+                            value: 50,
+                            message: "Message should be at least 50 characters"
+                        }
+                    })}
+                    className='w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg'
+                />
+                {
+                    errors.Message && <span className='inline-block self-start text-accent'>{errors.Message.message}</span>
+                }
 
-            <input
-                value="Cast your Message!"
-                className='px-10 py-4 rounded-md shadow-lg bg-background border border-accent/30 border-solid hover:shadow-glass-sm backdrop-blur-sm text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 cursor-pointer capitalize' type="submit" />
-        </form>
+                <input
+                    value="Cast your Message!"
+                    className='px-10 py-4 rounded-md shadow-lg bg-background border border-accent/30 border-solid hover:shadow-glass-sm backdrop-blur-sm text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 cursor-pointer capitalize' type="submit" />
+            </form>
         </>
-       
+
     );
 }
