5b785d95e6efd03a767fe7fbf1fa70e378322d08
Kenneth Amiel Santos <kennethamiel.santos@gmail.com>
2025-01-10 22:33:04 +0800

    DevTrack: Initial commit with existing project files

--- src/components/navigation/NavButton.jsx
+++ src/components/navigation/NavButton.jsx
@@ -1,58 +1,124 @@
-import { Home, Linkedin, Github, Palette, Phone, Twitter, User, NotebookText } from 'lucide-react'
-import Link from 'next/link'
-import React from 'react'
+import {
+  Github,
+  Home,
+  Linkedin,
+  NotebookText,
+  Palette,
+  Phone,
+  Twitter,
+  User,
+} from "lucide-react";
+import Link from "next/link";
+import React from "react";
+import ResponsiveComponent from "../ResponsiveComponent";
+import clsx from "clsx";
+import { motion } from "framer-motion";
 
 const getIcon = (icon) => {
-    switch (icon) {
-        case "home":
-            return <Home className='w-full h-auto' strokeWidth={1.5} />;
-        case "about":
-            return <User className='w-full h-auto' strokeWidth={1.5} />;
-        case "projects":
-            return <Palette className='w-full h-auto' strokeWidth={1.5} />;
-        case "contact":
-            return <Phone className='w-full h-auto' strokeWidth={1.5} />;
-        case "github":
-            return <Github className='w-full h-auto' strokeWidth={1.5} />;
-        case "linkedin":
-            return <Linkedin className='w-full h-auto' strokeWidth={1.5} />;
-        case "twitter":
-            return <Twitter className='w-full h-auto' strokeWidth={1.5} />;
-        case "resume":
-            return <NotebookText className='w-full h-auto' strokeWidth={1.5} />;
-        default:
-            return <Home className='w-full h-auto' strokeWidth={1.5} />
-    }
-}
+  switch (icon) {
+    case "home":
+      return <Home className="w-full h-auto" strokeWidth={1.5} />;
+    case "about":
+      return <User className="w-full h-auto" strokeWidth={1.5} />;
+    case "projects":
+      return <Palette className="w-full h-auto" strokeWidth={1.5} />;
+    case "contact":
+      return <Phone className="w-full h-auto" strokeWidth={1.5} />;
+    case "github":
+      return <Github className="w-full h-auto" strokeWidth={1.5} />;
+    case "linkedin":
+      return <Linkedin className="w-full h-auto" strokeWidth={1.5} />;
+    case "twitter":
+      return <Twitter className="w-full h-auto" strokeWidth={1.5} />;
+    case "resume":
+      return <NotebookText className="w-full h-auto" strokeWidth={1.5} />;
 
-const NavButton = ({ x, y, label, link, icon, newTab }) => {
-    return (
-        <div
-            className='absolute cursor-pointer z-50'
-            style={
-                {
-                    transform: `translate(${x}, ${y})`
-                }
-            }>
-            <Link
-                href={link}
-                target={newTab ? '_blank' : '_self'}
-                className='text-foreground rounded-full flex items-center justify-center custom-bg'
-                aria-label={label}
-                name={label}>
-                <span className='relative w-14 h-14 p-4 animate-spin-slow-reverse group-hover:pause hover:text-accent'>
-                    {getIcon(icon)}
+    default:
+      return <Home className="w-full h-auto" strokeWidth={1.5} />;
+  }
+};
 
-                    <span className='peer bg-transparent absolute top-0 left-0 w-full h-full' />
+const item = {
+  hidden: { scale: 0 },
+  show: { scale: 1 },
+};
 
-                    <span className='absolute hidden peer-hover:block px-2 py-1 left-full mx-2 top-1/2 -translate-y-1/2 bg-background text-foreground text-sm rounded-md shadow-lg whitespace-nowrap'>
-                        {label}
-                    </span>
+const NavLink = motion(Link);
+
+const NavButton = ({
+  x,
+  y,
+  label,
+  link,
+  icon,
+  newTab,
+  labelDirection = "right",
+}) => {
+  return (
+    <ResponsiveComponent>
+      {({ size }) => {
+        return size && size >= 480 ? (
+          <div
+            className="absolute cursor-pointer z-50"
+            style={{ transform: `translate(${x}, ${y})` }}
+          >
+            <NavLink
+              variants={item}
+              href={link}
+              target={newTab ? "_blank" : "_self"}
+              className="text-foreground  rounded-full flex items-center justify-center
+        custom-bg
+        "
+              aria-label={label}
+              name={label}
+              prefetch={false}
+              scroll={false}
+            >
+              <span className="relative  w-14 h-14 p-4 animate-spin-slow-reverse group-hover:pause hover:text-accent">
+                {getIcon(icon)}
+
+                <span className="peer bg-transparent absolute top-0 left-0 w-full h-full" />
+
+                <span className="absolute hidden peer-hover:block px-2 py-1 left-full mx-2 top-1/2 -translate-y-1/2 bg-background text-foreground text-sm rounded-md shadow-lg whitespace-nowrap">
+                  {label}
                 </span>
+              </span>
+            </NavLink>
+          </div>
+        ) : (
+          <div className="w-fit cursor-pointer z-50">
+            <NavLink
+              variants={item}
+              href={link}
+              target={newTab ? "_blank" : "_self"}
+              className="text-foreground  rounded-full flex items-center justify-center
+        custom-bg
+        "
+              aria-label={label}
+              name={label}
+              prefetch={false}
+              scroll={false}
+            >
+              <span className="relative  w-10 h-10  xs:w-14 xs:h-14 p-2.5 xs:p-4 hover:text-accent">
+                {getIcon(icon)}
 
-            </Link>
-        </div>
-    )
-}
+                <span className="peer bg-transparent absolute top-0 left-0 w-full h-full" />
 
-export default NavButton
+                <span
+                  className={clsx(
+                    "absolute hidden peer-hover:block px-2 py-1 left-full mx-2 top-1/2 -translate-y-1/2 bg-background text-foreground text-sm rounded-md shadow-lg whitespace-nowrap",
+                    labelDirection === "left" ? "right-full left-auto" : ""
+                  )}
+                >
+                  {label}
+                </span>
+              </span>
+            </NavLink>
+          </div>
+        );
+      }}
+    </ResponsiveComponent>
+  );
+};
+
+export default NavButton;
