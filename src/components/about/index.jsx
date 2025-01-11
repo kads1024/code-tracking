629c3e0da5ffafbb7794ea89fea4cde3b22931b5
Kenneth Amiel Santos <kennethamiel.santos@gmail.com>
2025-01-11 17:14:36 +0800

    Updated Github Streak

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -76,8 +76,8 @@ const AboutDetails = () => {
         <ItemLayout className={"col-span-full md:col-span-6 !p-0"}>
           <img
             className="w-full h-auto"
-            src={`${process.env.NEXT_PUBLIC_GITHUB_STREAK_STATS_URL}?user=codebucks27&theme=dark&hide_border=true&type=svg&background=EB545400&ring=FEFE5B&currStreakLabel=FEFE5B`}
-            alt="CodeBucks"
+            src={`${process.env.NEXT_PUBLIC_GITHUB_STREAK_STATS_URL}?user=kads1024&theme=dark&hide_border=true&type=svg&background=EB545400&ring=FEFE5B&currStreakLabel=FEFE5B`}
+            alt="Kenneth's Github Streak"
             loading="lazy"
           />
         </ItemLayout>
