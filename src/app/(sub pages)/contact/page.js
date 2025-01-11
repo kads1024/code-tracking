0a852dd3f8cf3f0139b7b4701da2cb991ef5bc9e
Kenneth Amiel Santos <kennethamiel.santos@gmail.com>
2025-01-11 16:36:30 +0800

    Updated contact page

--- src/app/(sub pages)/contact/page.js
+++ src/app/(sub pages)/contact/page.js
@@ -11,7 +11,7 @@ export default function Contact() {
     <>
       <Image
         src={bg}
-        alt="Next.js Portfolio website's contact page background image"
+        alt="Portfolio website's contact page background image"
         priority
         sizes="100vw"
         className="-z-50 fixed top-0 left-0 w-full h-full object-cover object-center opacity-50"
@@ -20,7 +20,7 @@ export default function Contact() {
       <article className="relative w-full flex flex-col items-center justify-center py-8 sm:py-0 space-y-8">
         <div className="flex flex-col items-center justify-center space-y-6 w-full sm:w-3/4">
           <h1 className="text-accent font-semibold text-center text-4xl capitalize">
-            summon the wizard
+            Summon the Wizard
           </h1>
           <p className="text-center font-light text-sm xs:text-base">
             Step into the circle of enchantment and weave your words into the
