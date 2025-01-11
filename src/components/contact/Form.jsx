8b463afc3bc68e253cd603be9cf53abbf4b76b3d
Kenneth Amiel Santos <kennethamiel.santos@gmail.com>
2025-01-11 17:21:42 +0800

    Updated To Name

--- src/components/contact/Form.jsx
+++ src/components/contact/Form.jsx
@@ -65,7 +65,7 @@ export default function Form() {
 
   const onSubmit = (data) => {
     const templateParams = {
-      to_name: "CodeBucks",
+      to_name: "Kenneth",
       from_name: data.name,
       reply_to: data.email,
       message: data.message,
