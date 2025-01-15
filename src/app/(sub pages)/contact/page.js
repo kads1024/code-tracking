a31974b25069ab7de30cc34b4a2adecdaa13db83
Kenneth Amiel Santos <kennethamiel.santos@gmail.com>
2025-01-16 00:50:46 +0800

    Update contact page

--- src/app/(sub pages)/contact/page.js
+++ src/app/(sub pages)/contact/page.js
@@ -20,7 +20,7 @@ export default function Contact() {
       <article className="relative w-full flex flex-col items-center justify-center py-8 sm:py-0 space-y-8">
         <div className="flex flex-col items-center justify-center space-y-6 w-full sm:w-3/4">
           <h1 className="text-accent font-semibold text-center text-4xl capitalize">
-            Invoke the Sorcerer's Presence
+            Invoke the Sorcerer`s Presence
           </h1>
           <p className="text-center font-light text-sm xs:text-base">
           Step forth, seeker of arcane wisdom and mystical craft, and send 
